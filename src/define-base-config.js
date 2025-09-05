7f96409df080b395b5b6c1904a6bf1427f98c9da
Artem Merkulov <merkulov.a@svgun.ru>
2025-09-05 16:33:32 +0300

    feat: update dependencies an rules

--- src/define-base-config.js
+++ src/define-base-config.js
@@ -4,7 +4,6 @@ import i18nextPlugin from 'eslint-plugin-i18next';
 import jestDomPlugin from 'eslint-plugin-jest-dom';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import playwrightPlugin from 'eslint-plugin-playwright';
-import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 import storybookPlugin from 'eslint-plugin-storybook';
 import testingLibraryPlugin from 'eslint-plugin-testing-library';
 
@@ -22,27 +21,11 @@ export default (config = []) =>
     },
     {
       plugins: {
-        'simple-import-sort': simpleImportSortPlugin,
         'jsx-a11y': jsxA11yPlugin,
         '18next': i18nextPlugin,
         storybook: storybookPlugin
       },
       rules: {
-        'simple-import-sort/exports': 'error',
-        'simple-import-sort/imports': [
-          'error',
-          {
-            groups: [
-              ['^react', '^@?\\w'],
-              ['^(@)(/.*|$)'],
-              ['^\\u0000'],
-              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-              ['^.+\\.?(css)$']
-            ]
-          }
-        ],
-        //
         'no-var': 'off',
         'no-undef': 'off',
         'no-unused-vars': 'off',
@@ -50,7 +33,9 @@ export default (config = []) =>
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/no-non-null-asserted-optional-chain': 'off'
+        '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+        //
+        'react-hooks-extra/no-unnecessary-use-prefix': 'off'
       }
     },
     {
