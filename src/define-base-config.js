519a8e0295abd594af87095fd910473d4d9c5b0b
Artem Merkulov <merkulov.a@svgun.ru>
2025-09-05 17:14:19 +0300

    feat: update eslint rules

--- src/define-base-config.js
+++ src/define-base-config.js
@@ -1,11 +1,11 @@
-import antfu from "@antfu/eslint-config";
-import prettierOptions from "@qtqtart/prettier-config";
-import i18nextPlugin from "eslint-plugin-i18next";
-import jestDomPlugin from "eslint-plugin-jest-dom";
-import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
-import playwrightPlugin from "eslint-plugin-playwright";
-import storybookPlugin from "eslint-plugin-storybook";
-import testingLibraryPlugin from "eslint-plugin-testing-library";
+import antfu from '@antfu/eslint-config';
+import prettierOptions from '@qtqtart/prettier-config';
+import i18nextPlugin from 'eslint-plugin-i18next';
+import jestDomPlugin from 'eslint-plugin-jest-dom';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import playwrightPlugin from 'eslint-plugin-playwright';
+import storybookPlugin from 'eslint-plugin-storybook';
+import testingLibraryPlugin from 'eslint-plugin-testing-library';
 
 export default (config = []) =>
   antfu(
@@ -15,42 +15,42 @@ export default (config = []) =>
       javascript: true,
       stylistic: false,
       formatters: {
-        prettierOptions,
+        prettierOptions
       },
-      ignores: ["dist", "node_modules"],
+      ignores: ['dist', 'node_modules']
     },
     {
       plugins: {
-        "jsx-a11y": jsxA11yPlugin,
-        "18next": i18nextPlugin,
-        storybook: storybookPlugin,
+        'jsx-a11y': jsxA11yPlugin,
+        '18next': i18nextPlugin,
+        storybook: storybookPlugin
       },
       rules: {
-        "jsonc/sort-keys": "off",
+        'jsonc/sort-keys': 'off',
         //
-        "no-var": "off",
-        "no-undef": "off",
-        "no-unused-vars": "off",
+        'no-var': 'off',
+        'no-undef': 'off',
+        'no-unused-vars': 'off',
         //
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
         //
-        "react-hooks-extra/no-unnecessary-use-prefix": "off",
-      },
+        'react-hooks-extra/no-unnecessary-use-prefix': 'off'
+      }
     },
     {
-      files: ["**/*.test.ts?(x)"],
-      ...testingLibraryPlugin.configs["flat/react"],
+      files: ['**/*.test.ts?(x)'],
+      ...testingLibraryPlugin.configs['flat/react']
     },
     {
-      files: ["**/*.test.ts?(x)"],
-      ...jestDomPlugin.configs["flat/recommended"],
+      files: ['**/*.test.ts?(x)'],
+      ...jestDomPlugin.configs['flat/recommended']
     },
     {
-      files: ["**/*.e2e.ts", "**/*.test.e2e.ts"],
-      ...playwrightPlugin.configs["flat/recommended"],
+      files: ['**/*.e2e.ts', '**/*.test.e2e.ts'],
+      ...playwrightPlugin.configs['flat/recommended']
     },
     ...config
   );
